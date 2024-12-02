b278c492fad108a9510fbc8111227ad8a1533c93
Rafael Becker <140275759+realfabecker@users.noreply.github.com>
2024-12-01 21:31:10 -0300

    Update and rename ansible.install.sh to ansible.deps.sh

--- ansible/ansible.deps.sh
+++ ansible/ansible.deps.sh
@@ -2,5 +2,4 @@
 sudo apt update \
  && sudo apt install software-properties-common -y \
  && sudo add-apt-repository --yes --update ppa:ansible/ansible \
- && sudo apt install ansible -y \
- && ansible-playbook --ask-become-pass ansible.deb.yml
+ && sudo apt install ansible -y
